392984d22b722486be8e3e362192b3e6434cdba9
eduard.parvu <parvueduard@gmail.com>
2018-11-09 00:27:57 +0200

    - init

---
+++ rust_projects/minigrep/src/main.rs
@@ -0,0 +1,41 @@
+use std::env;
+use std::fs;
+use std::process;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    let contents = fs::read_to_string(config.filename).unwrap_or_else(|err| {
+        println!("Problem reading file: {}", err.to_string());
+        process::exit(1);
+    });
+
+
+    println!("With text:\n{}", contents);
+}
+
+struct Config<'a> {
+    query: &'a str,
+    filename: &'a str,
+}
+
+impl<'a> Config<'a> {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments!");
+        }
+
+        let query = &args[1];
+        let filename = &args[2];
+
+        Ok(Config { query, filename } )
+    }
+}
