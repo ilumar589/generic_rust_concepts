75a63b61c48aff334b3bae7d24fe06b48ad19c66
eduard.parvu <parvueduard@gmail.com>
2018-11-11 17:17:18 +0200

    - refactor

--- rust_projects/minigrep/src/lib.rs
+++ rust_projects/minigrep/src/lib.rs
@@ -12,17 +12,22 @@ pub struct Config<'a> {
 }
 
 impl<'a> Config<'a> {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments!");
-        }
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = &args[1];
-        let filename = &args[2];
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string!")
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name!")
+        };
 
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
-        Ok(Config { query, filename, case_sensitive } )
+        Ok(Config { query, filename, case_sensitive })
     }
 }
 
@@ -44,27 +49,15 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
 }
 
 fn search<'a>(query: &str, contents: &'a str ) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) { // this should not because contains would destroy the memory for the passed query, so we should use &query
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(query.to_lowercase()))
+        .collect()
 }
